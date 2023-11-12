e77ac288527c45f900b6ae1b58a271be4e6b3525
awhfahim <fahimhasan314@gmail.com>
2023-11-12 12:08:37 +0600

    Created Logger Class for Single ton design Patten

---
+++ Design Patterns/Singleton Design Pattern/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton_Design_Pattern
+{
+    public class Logger
+    {
+        private static Logger _logger;
+
+        private Logger() { }
+        
+        private static List<string> logs = new List<string>();
+
+        public static Logger CreateLogger()
+        {
+            if (_logger == null)
+            {
+                _logger = new Logger();
+            }
+            return _logger;
+        }
+
+        public void Information(string message)
+        {
+            logs.Add(message);
+        }
+        
+        public void GetLogs()
+        {
+            foreach(var log in logs)
+            {
+                Console.WriteLine(log);
+            }
+        }
+    }
+}
--- Design Patterns/Singleton Design Pattern/Program.cs
+++ Design Patterns/Singleton Design Pattern/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using Singleton_Design_Pattern;
+
+var logger = Logger.CreateLogger();
+
+logger.Information("Hi my name is fahim");
+logger.Information("Fatal Error has been Occured");
+
+var logger2 = Logger.CreateLogger();
+
+logger2.Information("I am at Logger 2");
+
+logger.GetLogs();
