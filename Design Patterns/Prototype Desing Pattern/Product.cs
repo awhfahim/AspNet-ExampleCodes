efe6139ec70b9e311125b15b71eab101e3e7664d
awhfahim <fahimhasan314@gmail.com>
2023-11-12 12:17:51 +0600

    Added Code of Prototype Design Pattern

---
+++ Design Patterns/Prototype Desing Pattern/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype_Desing_Pattern
+{
+    public class Product : ICloneable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public object Clone()
+        {
+            Product p = new Product();
+            p.Name = Name;
+            p.Price = Price;
+            return p;
+        }
+    }
+}
--- Design Patterns/Prototype Desing Pattern/Program.cs
+++ Design Patterns/Prototype Desing Pattern/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Prototype_Desing_Pattern;
+
 Console.WriteLine("Hello, World!");
+Product product = new();
+product.Name = "Camera";
+product.Price = 100000;
+
+Product? product2 = product.Clone() as Product;
+
+Console.WriteLine($"{product2.Name} {product2.Price}");
