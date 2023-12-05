95682741109922e966816b10436de5477e7707b5
awhfahim <fahimhasan314@gmail.com>
2023-12-05 16:28:47 +0600

    ImplementingCleanArchitecture

---
+++ ImplementingCleanArchitecture/ImplementingCleanArchitecture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImplementingCleanArchitecture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
