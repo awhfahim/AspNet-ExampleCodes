204d8fa6751a882813e311225fbad368ffd2e7e3
Abu Wabaidar Hasan Fahim <fahimhasan314@gmail.com>
2023-12-09 18:33:20 +0600

    vvc

---
+++ WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using FlightBooking5.Data;
+using FlightBooking5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FlightBooking5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly FlightBooking5Context _context;
+
+        public HomeController(ILogger<HomeController> logger, FlightBooking5Context context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        
+
+      
+        public IActionResult Index()
+        {
+            var imageAds = _context.ImageAd.ToList();
+            return View(imageAds);
+           
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication4/Data/FlightBooking5Context.cs
@@ -0,0 +1,22 @@
+﻿using FlightBooking5.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightBooking5.Data
+{
+    public class FlightBooking5Context : IdentityDbContext
+    {
+        public FlightBooking5Context(DbContextOptions<FlightBooking5Context> options)
+            : base(options)
+        {
+        }
+
+        
+
+        public DbSet<FlightBooking5.Models.Flight>? Flight { get; set; }
+		public DbSet<FlightBooking5.Models.PaymentDetails> PaymentDetails { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; } 
+        public DbSet<FlightBooking5.Models.ImageAd>? ImageAd { get; set; }
+        public DbSet<FlightBooking5.Models.Employee>? Employee { get; set; }
+	}
+}
