d7c36d886ac1a60357e3e467133662868cec484d
awhfahim <fahimhasan314@gmail.com>
2023-12-09 18:42:41 +0600

    deleted ccv

--- WebApplication4/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FlightBooking5.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
