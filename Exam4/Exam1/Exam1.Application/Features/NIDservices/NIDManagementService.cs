f3e23aae9cc87bfe0289a6202c256810ff33ddd4
awhfahim <fahimhasan314@gmail.com>
2024-01-07 15:19:37 +0600

    practice num 4 for exam 1

---
+++ Exam4/Exam1/Exam1.Application/Features/NIDservices/NIDManagementService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using Exam1.Application.DTOs;
+using Exam1.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1.Application.Features.NIDservices
+{
+    public class NIDManagementService : INIDManagementService
+    {
+        private readonly IApplicationUnitOfWork _applicationUnitOfWork;
+        private readonly IMapper _mapper;
+
+        public NIDManagementService(IApplicationUnitOfWork applicationUnitOfWork, IMapper mapper)
+        {
+            _applicationUnitOfWork = applicationUnitOfWork;
+            _mapper = mapper;
+        }
+        public async Task CreateNidAsync(NIDcreateUpdateDTO nIDcreateUpdateDTO)
+        {
+            var nid = new NID();
+            _mapper.Map(nIDcreateUpdateDTO, nid);
+            await _applicationUnitOfWork.NIDRepository.AddAsync(nid);
+            await _applicationUnitOfWork.SaveAsync();
+        }
+
+        public async Task<(IList<NID> nIDs, int total, int totalDisplay)> GetTableDataAsync
+            (int pageIndex, int pageSize, string name, uint ageFrom, uint ageTo, string sortBy)
+        {
+            var r = await _applicationUnitOfWork.NIDRepository.GetTableDataAsync(pageIndex, pageSize, name, ageFrom, ageTo, sortBy);
+            return r;
+        }
+
+    }
+}
---
+++ Exam4/Exam1/Exam1.Web/WebModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using Exam1.Web.Areas.Admin.Models;
+
+namespace Exam1.Web
+{
+    public class WebModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CreateNidModel>().AsSelf();
+        }
+    }
+}
